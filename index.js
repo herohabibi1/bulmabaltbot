1e507958d6bfb0995dc8c2a5ea2376efbb9da279
herobest1 <shrawfact@gmail.com>
2025-11-13 13:08:04 +0530

    index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function createBot() {
       auth: config['bot-account']['type'],
       host: config.server.ip,
       port: config.server.port,
-      version: '1.20.4',
+      version: '1.20.1',
    });
 
    bot.loadPlugin(pathfinder);
